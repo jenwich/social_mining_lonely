6be7db5c7d8e6cda9e78d3495e5fae7fcf3f5240
Jenwich Rattanayenjai <pamjeavrat@gmail.com>
2017-04-28 20:29:09 +0700

    Warning in get_user.py

--- get_user.py
+++ get_user.py
@@ -1,6 +1,9 @@
 import pymongo
 from secret import *
 
+print("Do not run this file !!!!")
+exit(1)
+
 client = pymongo.MongoClient('mongodb://%s:%d/' % (MONGODB_HOST, MONGODB_PORT))
 db = client[MONGODB_DATABASE]
 db.authenticate(MONGODB_USERNAME, MONGODB_PASSWORD)
@@ -8,5 +11,6 @@ db.authenticate(MONGODB_USERNAME, MONGODB_PASSWORD)
 users= db.tweet.find({}, {'user': 1, 'created_at': 1}).sort('created_at', pymongo.ASCENDING)
 users = [i['user'] for i in list(users)]
 
+exit(1)
 for u in users:
     db.user.update({'_id': u['_id']}, u, upsert=True)
